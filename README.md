1e00e72bf0a57373e3ef74678fea829188bf4fd1
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-01-13 20:31:51 -0800

    update usage info to reflect surf integration

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ a mini ubersicht app
   Usage: /usr/bin/node -w path/to/my.widget [-e path1 -e path2]
     -w widget path
     -e extra file paths to watch and reload for
+    -s open and auto-reload surf browser (use v0.7 or newer)
 ```
--- index.js
+++ index.js
@@ -18,6 +18,7 @@ if (!wPath || args.h || args.help) {
   Usage: ${args._[0]} -w path/to/my.widget [-e path1 -e path2]
     -w widget path
     -e extra file paths to watch and reload for
+    -s open and auto-reload surf browser (use v0.7 or newer)
   `);
   process.exit(1);
 }
